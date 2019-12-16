2784cc53f5604850cd5d15988d251a09206d846e
Student <example@example.com>
2019-12-16 08:43:27 -0800

    add initial template repo

---
+++ lib/album.rb
@@ -0,0 +1,24 @@
+class Album
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+# 
+# all(): returns all albums;
+# find(): finds an album by its name;
+# find_by_ranking(): finds an album by its ranking (if we were to add a rank attribute to our class);
+# top_rated(): finds the top-rated album.
